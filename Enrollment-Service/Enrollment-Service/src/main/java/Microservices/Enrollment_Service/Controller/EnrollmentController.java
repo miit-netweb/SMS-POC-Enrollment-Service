5b0d265a7832d3b44472755dcf01762d045668d8
Abinash Patra <abinash.patra@nwbrd.com>
2024-07-26 15:32:21 +0530

    Partner Service Proxy added

--- Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/EnrollmentController.java
+++ Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/EnrollmentController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 import Microservices.Enrollment_Service.Dto.EmailMessageDto;
 import Microservices.Enrollment_Service.Dto.PartnerServiceDto;
 import Microservices.Enrollment_Service.Dto.SubscriberDto;
+import Microservices.Enrollment_Service.Dto.ThirdPartyEntityDto;
 import Microservices.Enrollment_Service.Publisher.RabbitMQProducer;
 import Microservices.Enrollment_Service.Service.AuthService;
 import Microservices.Enrollment_Service.proxy.JwtServerProxy;
 import Microservices.Enrollment_Service.proxy.PartnerServiceProxy;
+import Microservices.Enrollment_Service.proxy.ThirdPartyEntityProxy;
 
 @RestController
 @RequestMapping("/enroll")
@@ -33,6 +35,9 @@ public class EnrollmentController {
 	@Autowired
 	private PartnerServiceProxy partnerProxy;
 	@Autowired
+	private ThirdPartyEntityProxy thirdPartyProxy;
+
+	@Autowired
 	private RabbitMQProducer rabbitMQProducer;
 
 	@PostMapping("/subscriber")
@@ -46,7 +51,6 @@ public class EnrollmentController {
 					new PartnerServiceDto(user.getPartnerCredential(),
 							user.getEnrollmentDetail().getSubscriptionData()),
 					user.getEnrollmentDetail().getPartnerNumber());
-
 			if (responseDto.getBody() == true) {
 				LOGGER.info("partner number-> {} validated successfully",
 						user.getEnrollmentDetail().getPartnerNumber());
@@ -54,15 +58,31 @@ public class EnrollmentController {
 						.tokenGenerationForPartner(user.getEnrollmentDetail().getPartnerNumber());
 				LOGGER.info("JWT Token generated successfully for user of partner number-> {}",
 						user.getEnrollmentDetail().getPartnerNumber());
-				EmailMessageDto emailMessageDto = new EmailMessageDto(
-						user.getEnrollmentDetail().getSubscriberData().getEmail(),
-						user.getEnrollmentDetail().getSubscriberData().getFirstName(),
-						user.getEnrollmentDetail().getSubscriberData().getLastName(), 99995555L, 800);
-				CompletableFuture.runAsync(() -> rabbitMQProducer.sendMessage(emailMessageDto));
-				LOGGER.info("returning response entity");
-				LOGGER.info(Thread.currentThread().getName());
+				// Subscriber Number Generated
+				final String SUBSCRIBER_NUMBER = service.generateRandomAlphaNumeric();
+				LOGGER.info("Generated Subscriber Number {}", SUBSCRIBER_NUMBER);
 
-				return ResponseEntity.ok(tokenGenerationResponseDto);
+				ResponseEntity<Boolean> thirdPartyResponse = thirdPartyProxy.createCustomer(
+						new ThirdPartyEntityDto(user.getEnrollmentDetail().getPartnerNumber(), SUBSCRIBER_NUMBER,
+								user.getEnrollmentDetail().getSubscriptionData().getSubtypeNumber()));
+
+				if (thirdPartyResponse.getBody() == true) {
+//================================================================================================
+//===========================    EMAIL SERVICE     ===============================================
+					EmailMessageDto emailMessageDto = new EmailMessageDto(
+							user.getEnrollmentDetail().getSubscriberData().getEmail(),
+							user.getEnrollmentDetail().getSubscriberData().getFirstName(),
+							user.getEnrollmentDetail().getSubscriberData().getLastName(), 99995555L, 800);
+					CompletableFuture.runAsync(() -> rabbitMQProducer.sendMessage(emailMessageDto));
+					LOGGER.info("returning response entity");
+//=================================   ENDED  =========================================================
+//====================================================================================================
+					return ResponseEntity.ok(tokenGenerationResponseDto);
+				} else {
+					LOGGER.error("Failed to create Subscriber at Third Party Entity with Subscriber Number->{}",
+							SUBSCRIBER_NUMBER);
+					return ResponseEntity.badRequest().body("Failed to create Subscriber at Third Party Entity");
+				}
 			} else {
 				LOGGER.error("Invalid partner number-> {} for user enrollment",
 						user.getEnrollmentDetail().getPartnerNumber());
