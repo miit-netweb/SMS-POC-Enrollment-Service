57aee29827e210428c265186fc61e0207160b1bd
Miit Thakar <miit.thakar@nwbrd.com>
2024-08-05 15:05:44 +0530

    done some changes

--- Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/V2EnrollmentController.java
+++ Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/V2EnrollmentController.java
@@ -15,12 +15,14 @@ import Microservices.Enrollment_Service.proxy.ThirdPartyEntityProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
+
+//optional
+//slpunk
+//datadog
 
 @RestController
 @RequestMapping("/v2/enroll")
