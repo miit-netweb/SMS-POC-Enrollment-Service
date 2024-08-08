9391907d61a84203ab1c660ab0f25053a74550cc
Miit Thakar <miit.thakar@nwbrd.com>
2024-08-08 17:41:24 +0530

    Final Log updates

--- Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/V2EnrollmentController.java
+++ Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/V2EnrollmentController.java
@@ -9,6 +9,7 @@ import Microservices.Enrollment_Service.Publisher.RabbitMQProducer;
 import Microservices.Enrollment_Service.Service.AuthService;
 import Microservices.Enrollment_Service.Service.BillingService;
 import Microservices.Enrollment_Service.Service.EmailService;
+import Microservices.Enrollment_Service.exception.ValidationException;
 import Microservices.Enrollment_Service.proxy.JwtServerProxy;
 import Microservices.Enrollment_Service.proxy.PartnerServiceProxy;
 import Microservices.Enrollment_Service.proxy.ThirdPartyEntityProxy;
@@ -20,9 +21,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
 
-//optional
-//slpunk
-//datadog
 
 @RestController
 @RequestMapping("/v2/enroll")
@@ -49,10 +47,12 @@ public class V2EnrollmentController {
 
 
     @PostMapping("/subscriber")
-    public ResponseEntity<?> addNewUser(@RequestBody SubscriberDto user, @RequestHeader(value = "Authorization", required = false) String token) {
+    public ResponseEntity<?> addNewUser(@RequestBody SubscriberDto user, @RequestHeader(value = "Authorization", required = false) String token) throws Exception {
         LOGGER.info("Started validating user");
 
+        LOGGER.info("Validating token for the partner number -> {}",user.getEnrollmentDetail().getPartnerNumber());
         ResponseEntity<?> responseEntity = jwtServerProxy.validateToken(token);
+        LOGGER.info("Token validation successful for partner number -> {}",user.getEnrollmentDetail().getPartnerNumber());
 
         if (service.ValidateResponse(user)) {
             LOGGER.info("Validation successful for user of partner number-> {}",
@@ -60,11 +60,12 @@ public class V2EnrollmentController {
 
          final ResponseEntity<SubscriptionData> subscriptionData;
 			try {
+                LOGGER.info("Started validation subtype for partner number -> {}",user.getEnrollmentDetail().getPartnerNumber());
 			subscriptionData = partnerProxy.validatePartner(
 					new PartnerServiceDto(user.getPartnerCredential(),
 							user.getEnrollmentDetail().getSubscriptionData()),
 					user.getEnrollmentDetail().getPartnerNumber());
-
+                LOGGER.info("Subtype validation successful for partner number -> {}",user.getEnrollmentDetail().getPartnerNumber());
 			}catch(RuntimeException ex) {
 			    LOGGER.error("Original exception: {}", ex.getMessage());
 				ExceptionResponse errorResponse = ExceptionResponse.fromJson(ex.getMessage());
@@ -84,19 +85,23 @@ public class V2EnrollmentController {
                 ResponseEntity<Boolean> thirdPartyResponse = thirdPartyProxy.createCustomer(
                         new ThirdPartyEntityDto(user.getEnrollmentDetail().getPartnerNumber(), SUBSCRIBER_NUMBER,
                                 user.getEnrollmentDetail().getSubscriptionData().getSubtypeNumber()));
-
+                LOGGER.info("Successful response from third party");
                 if (Boolean.TRUE.equals(thirdPartyResponse.getBody())) {
                     final Subscriber ENROLLED_SUBSCRIBER = service.enrollNewSubscriber(user, SUBSCRIBER_NUMBER);
 //================================================================================================
 //===========================    BILLING SERVICE    (1)  ===============================================
                     // ADD in Billing Pending Table   AND   KAFKA :
+
                     CompletableFuture.runAsync(()->{
                         BillingPending billingPending = new BillingPending(SUBSCRIBER_NUMBER, user.getEnrollmentDetail().getPartnerNumber(),
                                 subscriptionData.getBody().getSubtypeNumber(),
                                 subscriptionData.getBody().getPricingRoutine(), "BILLING-PENDING"
                         );
+                        LOGGER.info("Billing pending status database entry initiated for subscriber number -> {}",SUBSCRIBER_NUMBER);
                         billingService.saveBillingPendingEntry(billingPending);
+                        LOGGER.info("Billing pending status database entry completed for subscriber number -> {}",SUBSCRIBER_NUMBER);
                         billingProducer.sendMessage(billingPending);
+                        LOGGER.info("Billing detail send to billing service successfully for subscriber number -> {}",SUBSCRIBER_NUMBER);
                     });
 
 //=================================   ENDED  =========================================================
@@ -105,14 +110,17 @@ public class V2EnrollmentController {
 //================================================================================================
 //===========================    EMAIL SERVICE     (2) ===============================================
                     CompletableFuture.runAsync(() ->{
+                        LOGGER.info("Email pending status database entry initiated for subscriber number -> {}",SUBSCRIBER_NUMBER);
                         EmailPending emailPending = emailService.addPendingEntry(new EmailPending(SUBSCRIBER_NUMBER, user.getEnrollmentDetail().getSubscriberData().getEmail(), 800, "EMAIL_PENDING"));
+                        LOGGER.info("Email pending status database entry completed for subscriber number -> {}",SUBSCRIBER_NUMBER);
                         rabbitMQProducer.sendMessage(emailPending);
+                        LOGGER.info("Email detail send to Email service successfully for subscriber number -> {}",SUBSCRIBER_NUMBER);
                     });
-                    LOGGER.info("returning response entity");
+
 //=================================   ENDED  =========================================================
 //====================================================================================================
 
-
+                    LOGGER.info("returning response entity");
                     return ResponseEntity.ok("success");
                 } else {
                     LOGGER.error("Failed to create Subscriber at Third Party Entity with Subscriber Number->{}",
