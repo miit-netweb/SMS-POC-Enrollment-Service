484360e1f95dfbbd07199306edaf4cf26083e796
Abinash Patra <abinash.patra@nwbrd.com>
2024-08-20 17:08:24 +0530

    checking partner number from claims

--- Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/V2EnrollmentController.java
+++ Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/Controller/V2EnrollmentController.java
@@ -1,6 +1,25 @@
 package Microservices.Enrollment_Service.Controller;
 
-import Microservices.Enrollment_Service.Dto.*;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Microservices.Enrollment_Service.Dto.ExceptionResponse;
+import Microservices.Enrollment_Service.Dto.PartnerServiceDto;
+import Microservices.Enrollment_Service.Dto.SubscriberDto;
+import Microservices.Enrollment_Service.Dto.SubscriptionData;
+import Microservices.Enrollment_Service.Dto.ThirdPartyEntityDto;
 import Microservices.Enrollment_Service.Entity.BillingPending;
 import Microservices.Enrollment_Service.Entity.EmailPending;
 import Microservices.Enrollment_Service.Entity.Subscriber;
@@ -9,22 +28,12 @@ import Microservices.Enrollment_Service.Publisher.RabbitMQProducer;
 import Microservices.Enrollment_Service.Service.AuthService;
 import Microservices.Enrollment_Service.Service.BillingService;
 import Microservices.Enrollment_Service.Service.EmailService;
+import Microservices.Enrollment_Service.exception.ErrorCodes;
+import Microservices.Enrollment_Service.exception.UnAuthorizedUserException;
 import Microservices.Enrollment_Service.exception.ValidationException;
 import Microservices.Enrollment_Service.proxy.JwtServerProxy;
 import Microservices.Enrollment_Service.proxy.PartnerServiceProxy;
 import Microservices.Enrollment_Service.proxy.ThirdPartyEntityProxy;
-import io.jsonwebtoken.Claims;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Random;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/v2/enroll")
@@ -49,89 +58,109 @@ public class V2EnrollmentController {
 	@Autowired
 	private BillingService billingService;
 
-//TODO  check claims for validating the partner
-
 	@PostMapping("/subscriber")
 	public ResponseEntity<?> addNewUser(@RequestBody SubscriberDto user,
 			@RequestHeader(value = "Authorization", required = false) String token) throws Exception {
 		LOGGER.info("Started validating user");
 
-		ResponseEntity<?> responseEntity = jwtServerProxy.validateToken(token);
+		final ResponseEntity<?> responseEntity;
+		try {
+			// Token Service Call
+			responseEntity = jwtServerProxy.validateToken(token);
+		} catch (RuntimeException ex) {
+			LOGGER.error("Original exception: {}", ex.getMessage());
+			ExceptionResponse errorResponse = ExceptionResponse.fromJson(ex.getMessage());
+			throw new ValidationException(errorResponse.getErrorcode(), errorResponse.getMessage(),
+					errorResponse.getStatus());
+		}
 
-		if (service.ValidateResponse(user)) {
-			LOGGER.info("Validation successful for user of partner number-> {}",
-					user.getEnrollmentDetail().getPartnerNumber());
-
-			final ResponseEntity<SubscriptionData> subscriptionData;
-			try {
-				subscriptionData = partnerProxy.validatePartner(
-						new PartnerServiceDto(user.getEnrollmentDetail().getSubscriptionData()),
+		Object body = responseEntity.getBody();
+		Map<String, Object> responseBody = (Map<String, Object>) body;
+		String partnerNumber = Optional.ofNullable(responseBody).map(map -> (String) map.get("partnerNumber"))
+				.orElse(null);
+		Long partnerNumberFromUser = user.getEnrollmentDetail().getPartnerNumber();
+		if (partnerNumber != null && partnerNumber.equals(partnerNumberFromUser.toString())) {
+			if (service.ValidateResponse(user)) {
+				LOGGER.info("Validation successful for user of partner number-> {}",
 						user.getEnrollmentDetail().getPartnerNumber());
 
-			} catch (RuntimeException ex) {
-				LOGGER.error("Original exception: {}", ex.getMessage());
-				ExceptionResponse errorResponse = ExceptionResponse.fromJson(ex.getMessage());
-				throw new ValidationException(errorResponse.getErrorcode(), errorResponse.getMessage(),
-						errorResponse.getStatus());
-			}
+				final ResponseEntity<SubscriptionData> subscriptionData;
+				try {
+					// Partner Service Call
+					subscriptionData = partnerProxy.validatePartner(
+							new PartnerServiceDto(user.getEnrollmentDetail().getSubscriptionData()),
+							user.getEnrollmentDetail().getPartnerNumber());
 
-			if (subscriptionData.getBody() != null) {
-				LOGGER.info("partner number-> {} validated successfully",
-						user.getEnrollmentDetail().getPartnerNumber());
+				} catch (RuntimeException ex) {
+					LOGGER.error("Original exception: {}", ex.getMessage());
+					ExceptionResponse errorResponse = ExceptionResponse.fromJson(ex.getMessage());
+					throw new ValidationException(errorResponse.getErrorcode(), errorResponse.getMessage(),
+							errorResponse.getStatus());
+				}
 
-				// Subscriber Number Generated
-				final String SUBSCRIBER_NUMBER = AuthService.generateRandomAlphaNumeric();
-				LOGGER.info("Generated Subscriber Number {}", SUBSCRIBER_NUMBER);
+				if (subscriptionData.getBody() != null) {
+					LOGGER.info("partner number-> {} validated successfully",
+							user.getEnrollmentDetail().getPartnerNumber());
 
-				ResponseEntity<Boolean> thirdPartyResponse = thirdPartyProxy.createCustomer(
-						new ThirdPartyEntityDto(user.getEnrollmentDetail().getPartnerNumber(), SUBSCRIBER_NUMBER,
-								user.getEnrollmentDetail().getSubscriptionData().getSubtypeNumber()));
+					// Subscriber Number Generated
+					final String SUBSCRIBER_NUMBER = AuthService.generateRandomAlphaNumeric();
+					LOGGER.info("Generated Subscriber Number {}", SUBSCRIBER_NUMBER);
 
-				if (Boolean.TRUE.equals(thirdPartyResponse.getBody())) {
-					final Subscriber ENROLLED_SUBSCRIBER = service.enrollNewSubscriber(user, SUBSCRIBER_NUMBER);
+					ResponseEntity<Boolean> thirdPartyResponse = thirdPartyProxy.createCustomer(
+							new ThirdPartyEntityDto(user.getEnrollmentDetail().getPartnerNumber(), SUBSCRIBER_NUMBER,
+									user.getEnrollmentDetail().getSubscriptionData().getSubtypeNumber()));
+
+					if (Boolean.TRUE.equals(thirdPartyResponse.getBody())) {
+						final Subscriber ENROLLED_SUBSCRIBER = service.enrollNewSubscriber(user, SUBSCRIBER_NUMBER);
 
 //================================================================================================
-//===========================    BILLING SERVICE    (1)  ===============================================
-					// ADD in Billing Pending Table AND KAFKA :
-					CompletableFuture.runAsync(() -> {
-						BillingPending billingPending = new BillingPending(SUBSCRIBER_NUMBER,
-								user.getEnrollmentDetail().getPartnerNumber(),
-								subscriptionData.getBody().getSubtypeNumber(),
-								subscriptionData.getBody().getPricingRoutine(), "BILLING-PENDING");
-						billingService.saveBillingPendingEntry(billingPending);
-						billingProducer.sendMessage(billingPending);
-					});
+//===========================    BILLING SERVICE CALL   (1)  ===============================================
+						// ADD in Billing Pending Table AND KAFKA :
+						CompletableFuture.runAsync(() -> {
+							BillingPending billingPending = new BillingPending(SUBSCRIBER_NUMBER,
+									user.getEnrollmentDetail().getPartnerNumber(),
+									subscriptionData.getBody().getSubtypeNumber(),
+									subscriptionData.getBody().getPricingRoutine(), "BILLING-PENDING");
+							billingService.saveBillingPendingEntry(billingPending);
+							billingProducer.sendMessage(billingPending);
+						});
 
 //=================================   ENDED  =========================================================
 //====================================================================================================
 
 //================================================================================================
-//===========================    EMAIL SERVICE     (2) ===============================================
-					CompletableFuture.runAsync(() -> {
-						EmailPending emailPending = emailService.addPendingEntry(new EmailPending(SUBSCRIBER_NUMBER,
-								user.getEnrollmentDetail().getSubscriberData().getEmail(), 800, "EMAIL_PENDING"));
-						rabbitMQProducer.sendMessage(emailPending);
-					});
-					LOGGER.info("returning response entity");
+//===========================    EMAIL SERVICE  CALL   (2) ===============================================
+						CompletableFuture.runAsync(() -> {
+							EmailPending emailPending = emailService.addPendingEntry(new EmailPending(SUBSCRIBER_NUMBER,
+									user.getEnrollmentDetail().getSubscriberData().getEmail(), 800, "EMAIL_PENDING"));
+							rabbitMQProducer.sendMessage(emailPending);
+						});
+						LOGGER.info("returning response entity");
 //=================================   ENDED  =========================================================
 //====================================================================================================
 
-					return ResponseEntity.ok("success");
+						return ResponseEntity.ok("success");
+					} else {
+						LOGGER.error("Failed to create Subscriber at Third Party Entity with Subscriber Number->{}",
+								SUBSCRIBER_NUMBER);
+						throw new ValidationException(ErrorCodes.SUBSCRIBER_CREATION_FAILED.getErrorCode(),
+								ErrorCodes.SUBSCRIBER_CREATION_FAILED.getErrorMessage(), HttpStatus.BAD_REQUEST);
+					}
 				} else {
-					LOGGER.error("Failed to create Subscriber at Third Party Entity with Subscriber Number->{}",
-							SUBSCRIBER_NUMBER);
-					return ResponseEntity.badRequest().body("Failed to create Subscriber at Third Party Entity");
+					LOGGER.error("Invalid partner number-> {} for user enrollment",
+							user.getEnrollmentDetail().getPartnerNumber());
+					throw new ValidationException(ErrorCodes.INVALID_PARTNER.getErrorCode(),
+							ErrorCodes.INVALID_PARTNER.getErrorMessage(), HttpStatus.BAD_REQUEST);
 				}
 			} else {
-				LOGGER.error("Invalid partner number-> {} for user enrollment",
-						user.getEnrollmentDetail().getPartnerNumber());
-
-				// Todo add exception handling and error response
-				return ResponseEntity.badRequest().body("Partner Validation failed");
+				LOGGER.error("Validation failed for user");
+				throw new ValidationException(ErrorCodes.USER_VALIDATION_FAILED.getErrorCode(),
+						ErrorCodes.USER_VALIDATION_FAILED.getErrorMessage(), HttpStatus.BAD_REQUEST);
 			}
 		} else {
-			LOGGER.error("Validation failed for user");
-			return ResponseEntity.badRequest().body(service.ValidateResponse(user));
+			LOGGER.error("Authentication Failed");
+			throw new UnAuthorizedUserException(4445, "Token does not belong to the specified partner",
+					HttpStatus.UNAUTHORIZED);
 		}
 	}
 }
--- Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/handler/GlobalExceptionHandler.java
+++ Enrollment-Service/Enrollment-Service/src/main/java/Microservices/Enrollment_Service/handler/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import Microservices.Enrollment_Service.Dto.ErrorIDGenerator;
 import Microservices.Enrollment_Service.Dto.ExceptionResponse;
 import Microservices.Enrollment_Service.exception.DuplicateEntryException;
+import Microservices.Enrollment_Service.exception.UnAuthorizedUserException;
 import Microservices.Enrollment_Service.exception.ValidationException;
 
 @ControllerAdvice
@@ -28,6 +29,17 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(DuplicateEntryException.class)
 	public ResponseEntity<ExceptionResponse> handleDuplicateEntryException(DuplicateEntryException exception) {
+		ExceptionResponse response=new ExceptionResponse();
+		response.setErrorid(ErrorIDGenerator.getErrorId());
+		response.setErrorcode(exception.getErrorcode());
+		response.setMessage(exception.getMessage());
+		response.setStatus(exception.getStatus());
+		response.setTimestamp(LocalTime.now().toString());
+		return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(UnAuthorizedUserException.class)
+	public ResponseEntity<ExceptionResponse> handleUnAUthorizedUserException(UnAuthorizedUserException exception) {
 		ExceptionResponse response=new ExceptionResponse();
 		response.setErrorid(ErrorIDGenerator.getErrorId());
 		response.setErrorcode(exception.getErrorcode());
